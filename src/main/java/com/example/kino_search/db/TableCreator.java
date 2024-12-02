b06a429da1943f1129fc8ae60c9303089f9b1f91
s26777 <s26777@pjwstk.edu.pl>
2024-12-02 14:51:51 +0300

    saving film with details to db

--- src/main/java/com/example/kino_search/db/TableCreator.java
+++ src/main/java/com/example/kino_search/db/TableCreator.java
@@ -9,14 +9,20 @@ public class TableCreator {
 
     private static final Logger logger = Logger.getLogger(TableCreator.class.getName());
 
-    public static void createUsersTable() {
+    public static void createFilmTable() {
         String createTableSQL = """
-            CREATE TABLE IF NOT EXISTS users (
+            CREATE TABLE IF NOT EXISTS film (
                 id SERIAL PRIMARY KEY,
-                nickname VARCHAR(50) NOT NULL,
-                email VARCHAR(100) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                title VARCHAR(60) NOT NULL,
+                release_date DATE,
+                poster_url TEXT,
+                api_id INT UNIQUE NOT NULL,
+                runtime INT,
+                api_rating FLOAT,
+                rating FLOAT,
+                api_count INT,
+                count INT,
+                overvuw TEXT
             )
         """;
 
@@ -24,14 +30,14 @@ public class TableCreator {
              Statement statement = connection.createStatement()) {
 
             statement.execute(createTableSQL);
-            logger.info("Table 'users' created successfully or already exists.");
+            logger.info("Table 'film' created successfully or already exists.");
 
         } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error creating table 'users'", e);
+            logger.log(Level.SEVERE, "Error creating table 'film'", e);
         }
     }
 
     public static void main(String[] args) {
-        createUsersTable();
+        FilmService.fetchAndSaveFilm(550);
     }
 }
